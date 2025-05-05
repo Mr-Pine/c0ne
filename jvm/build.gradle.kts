baf8b3ca11969b6ec0a4887807ce875fcd136901
Mr-Pine <git@mr-pine.de>
2025-05-05 22:45:03 +0200

    refactor(jvm: setup): Kotlin setup

--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     id("java")
+    kotlin("jvm") version "2.1.20"
     application
 }
 
-group = "edu.kit.kastel.logic"
+group = "de.mr_pine"
 version = "1.0-SNAPSHOT"
 
 application {
-    mainModule = "edu.kit.kastel.vads.compiler"
-    mainClass = "edu.kit.kastel.vads.compiler.Main"
+    mainClass = "de.mr_pine.c0ne.MainKt"
 }
 
 repositories {
@@ -17,14 +17,16 @@ repositories {
 
 dependencies {
     implementation("org.jspecify:jspecify:1.0.0")
+    implementation("com.github.ajalt.clikt:clikt:5.0.3")
+    testImplementation(kotlin("test"))
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-java {
-    toolchain.languageVersion = JavaLanguageVersion.of(24)
-}
-
 tasks.test {
     useJUnitPlatform()
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(23)
 }
