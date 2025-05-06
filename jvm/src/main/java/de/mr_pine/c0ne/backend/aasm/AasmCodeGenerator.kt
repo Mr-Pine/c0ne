891d3187859ad4a43ae1d0b5bf7036d69d06df9b
Mr-Pine <git@mr-pine.de>
2025-05-06 21:49:53 +0200

    format(jvm: backend): x86 register allocation and code generation

--- jvm/src/main/java/de/mr_pine/c0ne/backend/aasm/AasmCodeGenerator.kt
+++ jvm/src/main/java/de/mr_pine/c0ne/backend/aasm/AasmCodeGenerator.kt
@@ -6,7 +6,7 @@ import edu.kit.kastel.vads.compiler.ir.node.*
 import edu.kit.kastel.vads.compiler.ir.util.NodeSupport
 
 
-class AasmCodeGenerator : CodeGenerator<AasmRegisterAllocator> {
+class AasmCodeGenerator : CodeGenerator<AasmRegisterAllocator.AasmRegisterAllocation> {
     companion object {
         private const val INDENTATION = "  "
     }
@@ -16,48 +16,50 @@ class AasmCodeGenerator : CodeGenerator<AasmRegisterAllocator> {
 
     override fun getAllocator() = AasmRegisterAllocator()
 
+    override fun postprocess(generation: String) = generation.toByteArray()
+
     context(builder: StringBuilder)
     override fun prologue() {
     }
 
-    context(builder: StringBuilder)
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
     override fun functionPrologue(name: String) {
         builder.appendLine("function $name {")
         indentationLevel++
     }
 
-    context(builder: StringBuilder)
-    override fun functionEpilogue() {
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
+    override fun functionEpilogue(name: String) {
         builder.appendLine("}")
         indentationLevel--
     }
 
-    context(builder: StringBuilder, registers: RegisterAllocator.RegisterAllocation)
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
     override fun processNode(node: AddNode) {
         processBinaryOp(node, "add")
     }
 
-    context(builder: StringBuilder, registers: RegisterAllocator.RegisterAllocation)
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
     override fun processNode(node: SubNode) {
         processBinaryOp(node, "sub")
     }
 
-    context(builder: StringBuilder, registers: RegisterAllocator.RegisterAllocation)
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
     override fun processNode(node: DivNode) {
         processBinaryOp(node, "div")
     }
 
-    context(builder: StringBuilder, registers: RegisterAllocator.RegisterAllocation)
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
     override fun processNode(node: ModNode) {
         processBinaryOp(node, "mod")
     }
 
-    context(builder: StringBuilder, registers: RegisterAllocator.RegisterAllocation)
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
     override fun processNode(node: MulNode) {
         processBinaryOp(node, "mul")
     }
 
-    context(builder: StringBuilder, registers: RegisterAllocator.RegisterAllocation)
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
     fun processBinaryOp(node: BinaryOperationNode, opcode: String) {
         builder.indent()
             .append(registers[node])
@@ -70,7 +72,7 @@ class AasmCodeGenerator : CodeGenerator<AasmRegisterAllocator> {
             .appendLine()
     }
 
-    context(builder: StringBuilder, registers: RegisterAllocator.RegisterAllocation)
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
     override fun processNode(node: ConstIntNode) {
         builder.indent()
             .append(registers[node])
@@ -78,15 +80,15 @@ class AasmCodeGenerator : CodeGenerator<AasmRegisterAllocator> {
             .appendLine()
     }
 
-    context(builder: StringBuilder, registers: RegisterAllocator.RegisterAllocation)
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
     override fun processNode(node: Block) {
     }
 
-    context(builder: StringBuilder, registers: RegisterAllocator.RegisterAllocation)
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
     override fun processNode(node: ProjNode) {
     }
 
-    context(builder: StringBuilder, registers: RegisterAllocator.RegisterAllocation)
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
     override fun processNode(node: ReturnNode) {
         builder.indent()
             .append("ret ")
@@ -94,7 +96,7 @@ class AasmCodeGenerator : CodeGenerator<AasmRegisterAllocator> {
             .appendLine()
     }
 
-    context(builder: StringBuilder, registers: RegisterAllocator.RegisterAllocation)
+    context(builder: StringBuilder, registers: AasmRegisterAllocator.AasmRegisterAllocation)
     override fun processNode(node: StartNode) {
     }
 }
