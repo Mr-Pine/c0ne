182d8bdea25cc9ab078341b2023a3df90024391f
Mr-Pine <git@mr-pine.de>
2025-05-07 12:30:16 +0200

    test(jvm): Add tests

---
+++ jvm/src/test/kotlin/ParserTest.kt
@@ -0,0 +1,162 @@
+import edu.kit.kastel.vads.compiler.parser.ParseException
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ParserTest {
+    @Test
+    fun `program trailing stuff`() {
+        val expected = 42;
+        val program = """
+            int main() {
+                return $expected;
+            }
+            afasf
+            /
+        """.trimIndent()
+        assertThrows<ParseException> {
+            val result = TestUtil.compileAndRun(program)
+            assertEquals(expected % 256, result)
+        }
+    }
+
+    @Test
+    fun `multiple functions`() {
+        val expected = 42;
+        val program = """
+            int main() {
+                return $expected;
+            }
+            
+            int lol() {
+                return $expected;
+            }
+        """.trimIndent()
+        assertThrows<ParseException> {
+            val result = TestUtil.compileAndRun(program)
+            assertEquals(expected % 256, result)
+        }
+    }
+
+    @Test
+    fun `empty program`() {
+        val expected = 42;
+        val program = """
+        """.trimIndent()
+        assertThrows<ParseException> {
+            val result = TestUtil.compileAndRun(program)
+            assertEquals(expected % 256, result)
+        }
+    }
+
+    @Test
+    fun `lonely expression`() {
+        val expected = 42;
+        val program = """
+            int main() {
+                int a = 3;
+                a + 5;
+                return $expected;
+            }
+        """.trimIndent()
+        assertThrows<ParseException> {
+            val result = TestUtil.compileAndRun(program)
+            assertEquals(expected % 256, result)
+        }
+    }
+
+    @Test
+    fun `some lonely numbers`() {
+        val expected = 42;
+        val program = """
+            int main() {
+                a 3 5;
+                return $expected;
+            }
+        """.trimIndent()
+        assertThrows<ParseException> {
+            val result = TestUtil.compileAndRun(program)
+            assertEquals(expected % 256, result)
+        }
+    }
+
+    @Test
+    fun `missing semicolon`() {
+        val expected = 42;
+        val program = """
+            int main() {
+                int a = 3
+                return $expected;
+            }
+        """.trimIndent()
+        assertThrows<ParseException> {
+            val result = TestUtil.compileAndRun(program)
+            assertEquals(expected % 256, result)
+        }
+    }
+
+    @Test
+    fun `parenthesized lvalue`() {
+        val expected = 8;
+        val program = """
+            int main() {
+                int a = 0;
+                int b = 4;
+                (((((a))))) = b;
+                return a + b;
+            }
+        """.trimIndent()
+        val result = TestUtil.compileAndRun(program)
+        assertEquals(expected % 256, result)
+    }
+
+    @Test
+    fun `mismatched parenthesized lvalue`() {
+        val expected = 8;
+        val program = """
+            int main() {
+                int a = 0;
+                int b = 4;
+                (((((a)))) = b;
+                return a + b;
+            }
+        """.trimIndent()
+        assertThrows<ParseException> {
+            val result = TestUtil.compileAndRun(program)
+            assertEquals(expected % 256, result)
+        }
+    }
+
+    @Test
+    fun multiplication() {
+        val expected = -48;
+        val program = """
+            int main() {
+                int a = 3;
+                int b = 4;
+                int c = 4;
+                return a * b * -c;
+            }
+        """.trimIndent()
+        val result = TestUtil.compileAndRun(program)
+        assertEquals(expected.mod(256), result)
+    }
+
+    @Test
+    fun `assignment chain`() {
+        val expected = -48;
+        val program = """
+            int main() {
+                int a = 3;
+                int b = 4;
+                int c = 4;
+                a = b = c; 
+                return a * b * -c;
+            }
+        """.trimIndent()
+        assertThrows<ParseException> {
+            val result = TestUtil.compileAndRun(program)
+            assertEquals(expected.mod(256), result)
+        }
+    }
+}
---
+++ jvm/src/test/kotlin/TestUtil.kt
@@ -0,0 +1,50 @@
+import de.mr_pine.c0ne.backend.x86.X86CodeGenerator
+import edu.kit.kastel.vads.compiler.ir.SsaTranslation
+import edu.kit.kastel.vads.compiler.ir.optimize.LocalValueNumbering
+import edu.kit.kastel.vads.compiler.lexer.Lexer
+import edu.kit.kastel.vads.compiler.parser.Parser
+import edu.kit.kastel.vads.compiler.parser.TokenSource
+import edu.kit.kastel.vads.compiler.parser.ast.ProgramTree
+import edu.kit.kastel.vads.compiler.semantic.SemanticAnalysis
+import java.nio.file.attribute.PosixFilePermission
+import java.nio.file.attribute.PosixFilePermissions
+import kotlin.io.path.*
+
+class TestUtil {
+    companion object {
+        fun compileAndRun(input: String): Int {
+            val program = lexAndParse(input)
+            SemanticAnalysis(program).analyze()
+            val graphs = program.topLevelTrees.map { function ->
+                val translation = SsaTranslation(function, LocalValueNumbering())
+                translation.translate()
+            }
+
+            val code = X86CodeGenerator().generateCode(graphs)
+            val output = createTempFile(
+                "c0ne_test_output", "", PosixFilePermissions.asFileAttribute(
+                    setOf(
+                        PosixFilePermission.OWNER_EXECUTE,
+                        PosixFilePermission.OWNER_WRITE, PosixFilePermission.OWNER_READ
+                    )
+                )
+            )
+            output.writeBytes(code)
+            output.setPosixFilePermissions(
+                setOf(
+                    PosixFilePermission.OWNER_EXECUTE, PosixFilePermission.GROUP_EXECUTE,
+                    PosixFilePermission.OTHERS_EXECUTE
+                ) + output.getPosixFilePermissions()
+            )
+
+            return ProcessBuilder(output.absolutePathString()).start().waitFor()
+        }
+
+        private fun lexAndParse(input: String): ProgramTree {
+            val lexer = Lexer.forString(input)
+            val tokenSource = TokenSource(lexer)
+            val parser = Parser(tokenSource)
+            return parser.parseProgram()
+        }
+    }
+}
