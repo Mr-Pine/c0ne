6feec2954967b4d609546d30332c260ff5648a6e
Mr-Pine <git@mr-pine.de>
2025-05-09 00:21:26 +0200

    feat(jvm: backend): Don't give Const int values their own registers

--- jvm/src/main/java/de/mr_pine/c0ne/backend/AllocationRequirement.kt
+++ jvm/src/main/java/de/mr_pine/c0ne/backend/AllocationRequirement.kt
@@ -1,6 +1,7 @@
 package de.mr_pine.c0ne.backend
 
 import edu.kit.kastel.vads.compiler.ir.node.Block
+import edu.kit.kastel.vads.compiler.ir.node.ConstIntNode
 import edu.kit.kastel.vads.compiler.ir.node.Node
 import edu.kit.kastel.vads.compiler.ir.node.ProjNode
 import edu.kit.kastel.vads.compiler.ir.node.ReturnNode
@@ -8,4 +9,4 @@ import edu.kit.kastel.vads.compiler.ir.node.StartNode
 
 
 val Node.needsRegister: Boolean
-    get() = !(this is ProjNode || this is StartNode || this is Block || this is ReturnNode)
+    get() = !(this is ProjNode || this is StartNode || this is Block || this is ReturnNode || this is ConstIntNode)
