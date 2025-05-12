f04d9762e70ac967cf8daded7411e39602a71c70
Mr-Pine <git@mr-pine.de>
2025-05-12 22:58:56 +0200

    feat(jvm: semantic): Check for main Method

--- jvm/src/main/java/edu/kit/kastel/vads/compiler/semantic/SemanticAnalysis.java
+++ jvm/src/main/java/edu/kit/kastel/vads/compiler/semantic/SemanticAnalysis.java
@@ -1,7 +1,9 @@
 package edu.kit.kastel.vads.compiler.semantic;
 
+import de.mr_pine.c0ne.semantic.MainMethodAnalysis;
 import edu.kit.kastel.vads.compiler.parser.ast.ProgramTree;
 import edu.kit.kastel.vads.compiler.parser.visitor.RecursivePostorderVisitor;
+import kotlin.Unit;
 
 public class SemanticAnalysis {
 
@@ -15,6 +17,7 @@ public class SemanticAnalysis {
         this.program.accept(new RecursivePostorderVisitor<>(new IntegerLiteralRangeAnalysis()), new Namespace<>());
         this.program.accept(new RecursivePostorderVisitor<>(new VariableStatusAnalysis()), new Namespace<>());
         this.program.accept(new RecursivePostorderVisitor<>(new ReturnAnalysis()), new ReturnAnalysis.ReturnState());
+        this.program.accept(new RecursivePostorderVisitor<>(new MainMethodAnalysis()), Unit.INSTANCE);
     }
 
 }
