eb0db1770dabb655b432c968c12c6fefb2c82214
Mr-Pine <git@mr-pine.de>
2025-05-16 21:59:36 +0200

    feat(haskell: backend): SSA RegAlloc and Codegen

--- haskell/app/Compile/Backend/X86/Codegen.hs
+++ haskell/app/Compile/Backend/X86/Codegen.hs
@@ -1,28 +1,77 @@
-module Compile.Backend.X86.Codegen
-()
+module Compile.Backend.X86.Codegen (codeGen)
 where
-import Compile.Backend.X86.RegAlloc (RegisterMap, allocate)
-import Control.Monad.State
-import Compile.AST (AST (Block), Stmt)
+
+import Compile.Backend.Schedule (Schedule)
+import Compile.Backend.X86.RegAlloc (Register, RegisterMap, allocate, overflowCount, print)
+import Compile.IR.SSA (Node (Return, Start, Value), Value (ConstInt, BinaryOperation), BinaryOperation (Add, Sub, Mul, Div, Mod))
+import qualified Compile.IR.SSA as SSA
+import Control.Monad.State (State, execState, modify, gets)
+import qualified Data.Map as Map
+import Prelude hiding (print)
 
 type CodeGen a = State CodeGenState a
 
 data CodeGenState = CodeGenState
-  { regMap :: RegisterMap
-  , code :: [String]
-  }
+    { regMap :: RegisterMap
+    , code :: [String]
+    }
 
-codeGen :: AST -> [String]
-codeGen block@(Block stmts _) = code $ execState (genBlock stmts) initialState
+codeGen :: Schedule -> [String]
+codeGen schedule = code $ execState (genNodes schedule) initialState
   where
-    registerMap = allocate block
-    initialState = CodeGenState registerMap []
+    registerMap = allocate schedule
+    initialState = CodeGenState registerMap (prologue $ overflowCount registerMap)
 
-genBlock :: [Stmt] -> CodeGen ()
-genBlock = mapM_ genStmt
+prologue :: Int -> [String]
+prologue overflow = [".intel_syntax noprefix", ".global _start", ".text", "", "_start:", "call main", "mov edi, eax", "mov rax, 0x3c", "syscall", "", ".global main", "main:", "ENTER " ++ show (overflow * 4) ++ ", 0"]
+
+registerFor :: Node -> CodeGen Register
+registerFor node = do
+    registerMap <- gets regMap
+    return $ registerMap Map.! node
 
 emit :: String -> CodeGen ()
-emit instr = modify $ \s -> s {code = code s ++ [instr]}
+emit instr = modify $ \s -> s{code = code s ++ [instr]}
 
-genStmt:: Stmt -> CodeGen ()
-genStmt = error "a"
+genNodes :: [Node] -> CodeGen ()
+genNodes = mapM_ genNode
+
+genNode :: Node -> CodeGen ()
+genNode (Start _) = return ()
+genNode (Return result _) = do
+    resReg <- registerFor result
+    emit $ "MOV eax, " ++ print resReg
+    emit "LEAVE"
+    emit "RET"
+    return ()
+genNode node@(Value val _) = genValue val
+    where
+        genValue (ConstInt int) = do
+            target <- registerFor node
+            emit $ "MOV " ++ print target ++ ", " ++ show int
+        genValue (BinaryOperation binop) = do
+            target <- registerFor node
+            leftReg <- registerFor $ SSA.left binop
+            emit $ "MOV eax, " ++ print leftReg
+            rightReg <- registerFor $ SSA.right binop
+            genBinop binop rightReg target
+        genBinop (Add _ _) rightReg target = do
+            emit $ "ADD eax, " ++ print rightReg
+            emit $ "MOV " ++ print target ++ ", eax"
+        genBinop (Sub _ _) rightReg target = do
+            emit $ "SUB eax, " ++ print rightReg
+            emit $ "MOV " ++ print target ++ ", eax"
+        genBinop (Mul _ _) rightReg target = do
+            emit $ "IMUL eax, " ++ print rightReg
+            emit $ "MOV " ++ print target ++ ", eax"
+        genBinop (Div {}) rightReg target = do
+            emit "CDQ"
+            emit $ "MOV r15d, " ++ print rightReg
+            emit "IDIV r15d"
+            emit $ "MOV " ++ print target ++ ", eax"
+        genBinop (Mod {}) rightReg target = do
+            emit "CDQ"
+            emit $ "MOV r15d, " ++ print rightReg
+            emit "IDIV r15d"
+            emit $ "MOV " ++ print target ++ ", edx"
+
