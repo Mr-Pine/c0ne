aa50c66e0613701748f2a13f00c663af44f159c0
Mr-Pine <git@mr-pine.de>
2025-05-16 21:59:36 +0200

    feat(haskell: backend): SSA RegAlloc and Codegen

--- haskell/app/Compile/Backend/X86/RegAlloc.hs
+++ haskell/app/Compile/Backend/X86/RegAlloc.hs
@@ -1,18 +1,33 @@
-module Compile.Backend.X86.RegAlloc (allocate, RegisterMap, Register)
+module Compile.Backend.X86.RegAlloc (allocate, RegisterMap, overflowCount, Register, print)
 where
 
-import Compile.AST (AST (Block), Identifier, Stmt (Decl, Init))
 import Control.Monad.State (MonadState (get, put), State, execState)
 import qualified Data.Map as Map
+import Compile.Backend.Schedule (Schedule)
+import Compile.IR.SSA (Node)
+import Data.Maybe (mapMaybe, fromMaybe)
+import Data.List.Extra (lower)
+import Prelude hiding (print)
+import Safe.Foldable (maximumMay)
 
-data Register = EAX | EBX | ECX | EDX | ESI | EDI | RSP | RBP | R8D | R9D | R10D | R11D | R12D | R13D | R14D | R15D | OverflowSlot {index :: Int}
+data Register = EAX | EBX | ECX | EDX | ESI | EDI | RSP | RBP | R8D | R9D | R10D | R11D | R12D | R13D | R14D | R15D | OverflowSlot {index :: Int} deriving (Show, Eq)
+
+print :: Register -> String
+print (OverflowSlot idx) = "DWORD PTR [RSP + " ++ show (idx * 4) ++ "]"
+print reg = lower $ show reg
 
 allocateableRegisters :: [Register]
 allocateableRegisters = [EAX, EBX, ECX, ESI, EDI, R8D, R9D, R10D, R11D, R12D, R13D, R14D] ++ map OverflowSlot [1 ..]
 
-type RegisterMap = Map.Map Identifier Register
+type RegisterMap = Map.Map Node Register
 data RegAllocState = RegAllocState {remainingRegisters :: [Register], regMap :: RegisterMap}
 type RegAlloc a = State RegAllocState a
+
+overflowCount :: RegisterMap -> Int
+overflowCount = fromMaybe 0 . maximumMay . mapMaybe getIndex . Map.elems
+    where
+        getIndex (OverflowSlot idx) = Just idx
+        getIndex _ = Nothing
 
 nextReg :: RegAlloc Register
 nextReg = do
@@ -22,25 +37,19 @@ nextReg = do
     put curr{remainingRegisters = tail remaining}
     return next
 
-addMapping :: Identifier -> Register -> RegAlloc ()
-addMapping ident reg = do
+addMapping :: Node -> Register -> RegAlloc ()
+addMapping node reg = do
     curr <- get
     let registerMap = regMap curr
-    put curr{regMap = Map.insert ident reg registerMap}
+    put curr{regMap = Map.insert node reg registerMap}
 
-allocate :: AST -> RegisterMap
-allocate (Block statements _) = regMap $ execState (allocateBlock statements) initialState
+allocate :: Schedule -> RegisterMap
+allocate schedule = regMap $ execState (allocateSchedule schedule) initialState
   where
     initialState = RegAllocState allocateableRegisters Map.empty
 
-allocateBlock :: [Stmt] -> RegAlloc ()
-allocateBlock = mapM_ allocateStatement
+allocateSchedule :: Schedule -> RegAlloc ()
+allocateSchedule = mapM_ allocateNode
 
-allocateStatement :: Stmt -> RegAlloc ()
-allocateStatement (Decl ident _) = do
-    reg <- nextReg
-    addMapping ident reg
-allocateStatement (Init ident _ _) = do
-    reg <- nextReg
-    addMapping ident reg
-allocateStatement _ = return ()
+allocateNode :: Node -> RegAlloc ()
+allocateNode node = nextReg >>= addMapping node
