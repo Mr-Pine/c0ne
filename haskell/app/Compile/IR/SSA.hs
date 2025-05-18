4e2136c4c67ce409b9446740a1be85322d298f33
Mr-Pine <git@mr-pine.de>
2025-05-18 11:26:44 +0200

    perf(haskell: ir): Number SSA Nodes

--- haskell/app/Compile/IR/SSA.hs
+++ haskell/app/Compile/IR/SSA.hs
@@ -15,9 +15,9 @@ data Block
     deriving (Ord, Eq, Show)
 
 data Node
-    = Value {value :: Value, block :: Block}
-    | Start {block :: Block}
-    | Return {result :: Node, sideEffect :: Node, block :: Block}
+    = Value {value :: Value, block :: Block, id :: Int}
+    | Start {block :: Block, id :: Int}
+    | Return {result :: Node, sideEffect :: Node, block :: Block, id :: Int}
     deriving (Ord, Eq, Show)
 
 data Value = ConstInt Integer | BinaryOperation BinaryOperation deriving (Ord, Eq, Show)
@@ -28,18 +28,25 @@ data BinaryOperation
     | Mul {left :: Node, right :: Node}
     | Mod {left :: Node, right :: Node, sideEffect :: Node}
     | Div {left :: Node, right :: Node, sideEffect :: Node}
-    deriving (Ord, Eq, Show)
+    deriving (Ord, Eq)
+
+instance Show BinaryOperation where
+    show (Add l r)   = "Add {leftId=" ++ show l.id ++ ", rightId=" ++ show r.id ++ "}"
+    show (Sub l r)   = "Sub {leftId=" ++ show l.id ++ ", rightId=" ++ show r.id ++ "}"
+    show (Mul l r)   = "Mul {leftId=" ++ show l.id ++ ", rightId=" ++ show r.id ++ "}"
+    show (Mod l r s) = "Mod {leftId=" ++ show l.id ++ ", rightId=" ++ show r.id ++ ", sideEffectId=" ++ show s.id ++ "}"
+    show (Div l r s) = "Div {leftId=" ++ show l.id ++ ", rightId=" ++ show r.id ++ ", sideEffectId=" ++ show s.id ++ "}"
 
 maybeSideEffect :: Node -> Maybe Node
-maybeSideEffect (Value (BinaryOperation o@(Div {})) _) = Just o.sideEffect
-maybeSideEffect (Value (BinaryOperation o@(Mod {})) _) = Just o.sideEffect
+maybeSideEffect (Value (BinaryOperation o@(Div {})) _ _) = Just o.sideEffect
+maybeSideEffect (Value (BinaryOperation o@(Mod {})) _ _) = Just o.sideEffect
 maybeSideEffect n@(Return {}) = Just n.sideEffect
 maybeSideEffect _ = Nothing
 
 type Successors = Map.Map Node (Set.Set Node)
 type ValueMapping = Map.Map Identifier Node
 
-data SSATranslationState = SSATranslationState {currentBlock :: Block, largestBlockId :: Int, currentSideEffectNode :: Node, translationSuccessors :: Successors, currentReturnNodes :: [Node], valueMapping :: ValueMapping}
+data SSATranslationState = SSATranslationState {currentBlock :: Block, largestBlockId :: Int, largestNodeId :: Int, currentSideEffectNode :: Node, translationSuccessors :: Successors, currentReturnNodes :: [Node], valueMapping :: ValueMapping}
 type SSATranslation a = State SSATranslationState a
 
 data SSAGraph = SSAGraph {successors :: Successors, returnNodes :: [Node]} deriving (Show)
@@ -49,8 +56,8 @@ ssaTransform functionName ast = SSAGraph (translationSuccessors resultState) (cu
   where
     astTranslation = translateAst ast
     initialBlock = FunctionBody functionName 0
-    startNode = Start initialBlock
-    initialFunctionState = SSATranslationState initialBlock 0 startNode Map.empty [] Map.empty
+    startNode = Start initialBlock 0
+    initialFunctionState = SSATranslationState initialBlock 0 0 startNode Map.empty [] Map.empty
 
     resultState = execState astTranslation initialFunctionState
 
@@ -77,6 +84,14 @@ insertSideEffect node = do
     state <- get
     put state{currentSideEffectNode = node}
 
+createNode :: (Block -> Int -> Node) -> SSATranslation Node
+createNode c = do
+    block <- getCurrentBlock
+    nodeId <- gets largestNodeId
+    state <- get
+    put state{largestNodeId=nodeId + 1}
+    return $ c block (nodeId + 1)
+
 translateAst :: AST -> SSATranslation Node
 translateAst (AST.Block stmts _) = last . catMaybes <$> mapM translateStatements stmts
 
@@ -87,9 +102,8 @@ translateStatements (Asgn identifier Nothing expr _) = Just <$> translateAssignm
 translateStatements (Asgn identifier (Just op) expr sourcePos) = Just <$> translateAssignment identifier (BinExpr op (Ident identifier sourcePos) expr)
 translateStatements (Ret expr _) = do
     value <- translateExpression expr
-    block <- getCurrentBlock
     sideEffect <- gets currentSideEffectNode
-    let returnNode = Return value sideEffect block
+    returnNode <- createNode $ Return value sideEffect
     current <- get
     put current{currentReturnNodes=currentReturnNodes current ++ [returnNode]}
     addSuccessor value returnNode
@@ -99,24 +113,19 @@ translateAssignment :: Identifier -> Expr -> SSATranslation Node
 translateAssignment identifier expr = translateExpression expr >>= assignValue identifier
 
 translateExpression :: Expr -> SSATranslation Node
-translateExpression (IntExpr lit _) = do
-    block <- gets currentBlock
-    let valueNode = Value (ConstInt lit.value) block
-    return valueNode
+translateExpression (IntExpr lit _) = createNode $ Value (ConstInt lit.value)
 translateExpression (Ident ident _) = gets ((Map.! ident) . valueMapping)
 translateExpression (UnExpr Neg expr) = do
-    block <- gets currentBlock
-    let constZeroNode = Value (ConstInt 0) block
+    constZeroNode <- createNode $ Value (ConstInt 0)
     rightNode <- translateExpression expr
-    let exprNode = Value (BinaryOperation (Sub constZeroNode rightNode)) block
+    exprNode <- createNode $ Value (BinaryOperation (Sub constZeroNode rightNode))
     addSuccessor constZeroNode exprNode
     addSuccessor rightNode exprNode
     return exprNode
 translateExpression (BinExpr op lexpr rexpr) | op == AST.Sub || op == AST.Add || op == AST.Mul = do
-    block <- gets currentBlock
     leftNode <- translateExpression lexpr
     rightNode <- translateExpression rexpr
-    let exprNode = Value (BinaryOperation (operandMapping op leftNode rightNode)) block
+    exprNode <- createNode $ Value (BinaryOperation (operandMapping op leftNode rightNode))
     addSuccessor leftNode exprNode
     addSuccessor rightNode exprNode
     return exprNode
@@ -125,11 +134,10 @@ translateExpression (BinExpr op lexpr rexpr) | op == AST.Sub || op == AST.Add ||
     operandMapping AST.Add = Add
     operandMapping AST.Mul = Mul
 translateExpression (BinExpr op lexpr rexpr) | op == AST.Mod || op == AST.Div = do
-    block <- gets currentBlock
     leftNode <- translateExpression lexpr
     rightNode <- translateExpression rexpr
     sideEffect <- gets currentSideEffectNode
-    let exprNode = Value (BinaryOperation (operandMapping op leftNode rightNode sideEffect)) block
+    exprNode <- createNode $ Value (BinaryOperation (operandMapping op leftNode rightNode sideEffect))
     addSuccessor leftNode exprNode
     addSuccessor rightNode exprNode
     insertSideEffect exprNode
