0cc8187489972a15d14e021b987fb32cad9205e3
Mr-Pine <git@mr-pine.de>
2025-05-18 11:27:01 +0200

    fix(haskell: backend): Fixed scheduling using toposort

--- haskell/app/Compile/Backend/Schedule.hs
+++ haskell/app/Compile/Backend/Schedule.hs
@@ -7,13 +7,26 @@ import Data.List.NonEmpty (groupWith, head)
 import qualified Data.Set as Set
 import Prelude hiding (head)
 
+{-
+ - Scheduling Algorithm
+ -
+ - Do a toposort.
+ - As we have much nicer back edges, we do a reverse toposort and reverse the result
+ -}
+
 type Schedule = [Node]
 schedule :: SSAGraph -> Schedule
-schedule (SSAGraph successors returnNodes) = graphSchedule $ execState (scheduleBasicBlock exitPoint) initialState
+schedule (SSAGraph successors returnNodes) = toSchedule toposort
   where
     initialState = ScheduleConstructionState Set.empty []
     exitPoints = map head $ groupWith SSA.block returnNodes
     [exitPoint] = exitPoints
+
+    moveStartToStart xs = filter isStart xs ++ filter (not . isStart) xs
+    toSchedule = moveStartToStart . reverse
+    toposort = graphSchedule $ execState (scheduleBasicBlock exitPoint) initialState
+    isStart (Start {}) = True
+    isStart _ = False
 
 data ScheduleConstructionState = ScheduleConstructionState {visited :: Set.Set Node, graphSchedule :: [Node]}
 type ScheduleConstruction a = State ScheduleConstructionState a
@@ -31,16 +44,16 @@ isScheduled node = do
     return $ node `Set.member` visitedNodes
 
 scheduleBasicBlock :: Node -> ScheduleConstruction ()
-scheduleBasicBlock node@(Start _) = addNode node
-scheduleBasicBlock node@(Return result sideEffect _) = addNode node >> scheduleBasicBlock result >> scheduleBasicBlock sideEffect
-scheduleBasicBlock node@(Value val _) = scheduleValue val
+scheduleBasicBlock node@(Start _ _) = addNode node
+scheduleBasicBlock node@(Return result sideEffect _ _) = scheduleBasicBlock result >> scheduleBasicBlock sideEffect >> addNode node
+scheduleBasicBlock node@(Value val _ _) = scheduleValue val
   where
     scheduleValue (ConstInt _) = addNode node
     scheduleValue (BinaryOperation binop) = do
         alreadyScheduled <- isScheduled node
         unless alreadyScheduled $ do
-            addNode node
             scheduleBasicBlock $ SSA.right binop
             scheduleBasicBlock $ SSA.left binop
             let sideeffect = SSA.maybeSideEffect node
             forM_ sideeffect scheduleBasicBlock
+            addNode node
