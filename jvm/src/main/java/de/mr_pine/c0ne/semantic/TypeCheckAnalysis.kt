10e559fc34f1affaa3b3a26f9239b3b27f25f52f
Mr-Pine <git@mr-pine.de>
2025-05-23 21:46:53 +0200

    feat(jvm: semantic): Type checking

---
+++ jvm/src/main/java/de/mr_pine/c0ne/semantic/TypeCheckAnalysis.kt
@@ -0,0 +1,89 @@
+package de.mr_pine.c0ne.semantic
+
+import de.mr_pine.c0ne.parser.ast.AssignmentTree
+import de.mr_pine.c0ne.parser.ast.BinaryOperationTree
+import de.mr_pine.c0ne.parser.ast.DeclarationTree
+import de.mr_pine.c0ne.parser.ast.ForTree
+import de.mr_pine.c0ne.parser.ast.FunctionTree
+import de.mr_pine.c0ne.parser.ast.IfTree
+import de.mr_pine.c0ne.parser.ast.LValueIdentTree
+import de.mr_pine.c0ne.parser.ast.ReturnTree
+import de.mr_pine.c0ne.parser.ast.UnaryOperationTree
+import de.mr_pine.c0ne.parser.ast.WhileTree
+import de.mr_pine.c0ne.parser.type.BasicType
+import de.mr_pine.c0ne.parser.visitor.NoOpVisitor
+
+class TypeCheckAnalysis : NoOpVisitor<List<ReturnTree>> {
+    override fun visit(functionTree: FunctionTree, data: List<ReturnTree>) {
+        for (returnTree in data) {
+            if (returnTree.expression.type != functionTree.returnType.type) throw SemanticException("Return type ${returnTree.expression.type} at ${returnTree.span} does not match expected type ${functionTree.returnType.type}")
+        }
+
+        super.visit(functionTree, listOf())
+    }
+
+    override fun visit(returnTree: ReturnTree, data: List<ReturnTree>) {
+        super.visit(returnTree, data + listOf(returnTree))
+    }
+
+    override fun visit(
+        declarationTree: DeclarationTree, data: List<ReturnTree>
+    ) {
+        if (declarationTree.initializer != null) {
+            if (declarationTree.initializer.type != declarationTree.type) throw SemanticException("Type mismatch at ${declarationTree.span} for ${declarationTree.name.name} initializer: Expected ${declarationTree.type} got ${declarationTree.initializer.type}")
+        }
+        super.visit(declarationTree, data)
+    }
+
+    override fun visit(
+        assignmentTree: AssignmentTree, data: List<ReturnTree>
+    ) {
+        val variableType = (assignmentTree.lValue as LValueIdentTree).name.references!!.type
+        if (assignmentTree.expression.type != variableType) throw SemanticException("Type mismatch at ${assignmentTree.span} for ${assignmentTree.lValue.name.name}: Expected $variableType got ${assignmentTree.expression.type}")
+        super.visit(assignmentTree, data)
+    }
+
+    override fun visit(
+        binaryOperationTree: BinaryOperationTree, data: List<ReturnTree>
+    ) {
+        val lhsType = binaryOperationTree.lhs.type
+        val rhsType = binaryOperationTree.rhs.type
+
+        val inputType = binaryOperationTree.operatorType.inputType
+
+        if (inputType != null) {
+            if (lhsType != inputType) throw SemanticException("Type mismatch at ${binaryOperationTree.span} for ${binaryOperationTree.lhs}: Expected $inputType got $lhsType")
+            if (rhsType != inputType) throw SemanticException("Type mismatch at ${binaryOperationTree.span} for ${binaryOperationTree.rhs}: Expected $inputType got $rhsType")
+        }
+
+        if (lhsType != rhsType) throw SemanticException("Type mismatch at ${binaryOperationTree.span}: Could not unify types $lhsType and $rhsType")
+
+        super.visit(binaryOperationTree, data)
+    }
+
+    override fun visit(
+        unaryOperationTree: UnaryOperationTree, data: List<ReturnTree>
+    ) {
+        val expressionType = unaryOperationTree.expression.type
+        if (unaryOperationTree.operator.type.inputType != expressionType) throw SemanticException("Type mismatch at ${unaryOperationTree.span} for ${unaryOperationTree.expression}: Expected ${unaryOperationTree.operator.type.inputType} got $expressionType")
+        super.visit(unaryOperationTree, data)
+    }
+
+    override fun visit(ifTree: IfTree, data: List<ReturnTree>) {
+        if (ifTree.condition.type != BasicType.Boolean) throw SemanticException("Type mismatch at ${ifTree.span} for if condition: Expected ${BasicType.Boolean} got ${ifTree.condition.type}")
+
+        super.visit(ifTree, data)
+    }
+
+    override fun visit(whileTree: WhileTree, data: List<ReturnTree>) {
+        if (whileTree.condition.type != BasicType.Boolean) throw SemanticException("Type mismatch at ${whileTree.span} for while condition: Expected ${BasicType.Boolean} got ${whileTree.condition.type}")
+
+        super.visit(whileTree, data)
+    }
+
+    override fun visit(forTree: ForTree, data: List<ReturnTree>) {
+        if (forTree.condition.type != BasicType.Boolean) throw SemanticException("Type mismatch at ${forTree.span} for condition of for loop: Expected ${BasicType.Boolean} got ${forTree.condition.type}")
+
+        super.visit(forTree, data)
+    }
+}
