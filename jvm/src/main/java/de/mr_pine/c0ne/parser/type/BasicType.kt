81f470aa4176f6f1e81c75a9da3b20c99380e63a
Mr-Pine <git@mr-pine.de>
2025-05-25 01:05:17 +0200

    feat(jvm: semantic): Pretty print types with their representation

--- jvm/src/main/java/de/mr_pine/c0ne/parser/type/BasicType.kt
+++ jvm/src/main/java/de/mr_pine/c0ne/parser/type/BasicType.kt
@@ -3,8 +3,10 @@ package de.mr_pine.c0ne.parser.type
 interface BasicType: Type {
     object Integer: Type {
         override fun asString() = "int"
+        override fun toString() = asString()
     }
     object Boolean: Type {
         override fun asString() = "bool"
+        override fun toString() = asString()
     }
 }
