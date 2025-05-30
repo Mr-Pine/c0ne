a50040d27c7f60dbcc637a1b1f67f6dcb03a8a15
Mr-Pine <git@mr-pine.de>
2025-05-30 20:16:09 +0200

    feat(jvm: semantic): Proper return analysis

--- jvm/src/main/java/de/mr_pine/c0ne/semantic/SemanticAnalysis.kt
+++ jvm/src/main/java/de/mr_pine/c0ne/semantic/SemanticAnalysis.kt
@@ -2,8 +2,7 @@ package de.mr_pine.c0ne.semantic
 
 import de.mr_pine.c0ne.parser.ast.ProgramTree
 import de.mr_pine.c0ne.parser.visitor.RecursivePostorderVisitor
-import de.mr_pine.c0ne.parser.visitor.ReturnAnalysis
-import de.mr_pine.c0ne.parser.visitor.ReturnAnalysis.ReturnState
+import de.mr_pine.c0ne.semantic.ReturnAnalysis.ReturnState
 
 class SemanticAnalysis(private val program: ProgramTree) {
     fun analyze() {
