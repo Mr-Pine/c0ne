83e67fa81d11f1d6e2c73e7c2c85f90754a18565
Mr-Pine <git@mr-pine.de>
2025-06-02 13:27:24 +0200

    fix(jvm: backend): Sub mnemonic

--- jvm/src/main/java/de/mr_pine/c0ne/backend/x86/instructions/Sub.kt
+++ jvm/src/main/java/de/mr_pine/c0ne/backend/x86/instructions/Sub.kt
@@ -2,7 +2,7 @@ package de.mr_pine.c0ne.backend.x86.instructions
 
 import de.mr_pine.c0ne.backend.x86.NextGenSimpleX86RegAlloc
 
-class Sub private constructor(val left: Argument, val right: Argument) : Instruction("ADD", left, right) {
+class Sub private constructor(val left: Argument, val right: Argument) : Instruction("SUB", left, right) {
     constructor(left: Argument.RegMem.Register, right: Argument): this(left as Argument, right)
 
     context(alloc: NextGenSimpleX86RegAlloc)
