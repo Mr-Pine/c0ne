1a51bf0b50daf30fb3511a2c0a29dffc20c3a610
Mr-Pine <git@mr-pine.de>
2025-06-02 15:25:39 +0200

    fix(jvm: backend): Fix Or mnemonic

--- jvm/src/main/java/de/mr_pine/c0ne/backend/x86/instructions/Or.kt
+++ jvm/src/main/java/de/mr_pine/c0ne/backend/x86/instructions/Or.kt
@@ -2,7 +2,7 @@ package de.mr_pine.c0ne.backend.x86.instructions
 
 import de.mr_pine.c0ne.backend.x86.NextGenSimpleX86RegAlloc
 
-class Or private constructor(val left: Argument, val right: Argument) : Instruction("AND", left, right) {
+class Or private constructor(val left: Argument, val right: Argument) : Instruction("OR", left, right) {
     constructor(left: Argument.RegMem.Register, right: Argument): this(left as Argument, right)
 
     context(alloc: NextGenSimpleX86RegAlloc)
